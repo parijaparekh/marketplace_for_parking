348c670e5f29b439c447d88dfa15861ed9527d99
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-11-27 01:38:37 +0800

    Added listings of parkingslots and bookings. Data incomplete, will elaborate later.

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -2,18 +2,18 @@ const router = require('express').Router();
 const { User, ParkingSlot, SlotBooking, Address, LocationTag, ParkingSlotDates } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
       include: [{model: ParkingSlot}, 
-                {model: SlotBooking}],
+                {model: SlotBooking}]
     });
-    
     const user = userData.get({ plain: true });
     console.log(user);
-    res.render('profile', {
+
+    res.render('dashboard', {
       ...user,
       logged_in: true
     });
@@ -30,15 +30,10 @@ router.get('/sellaspot', withAuth, async (req, res) => {
   res.render('sellaspot', {logged_in: true});
 });
 
-router.get('/profile', withAuth, async (req, res) => {
-  res.render('profile', {logged_in: true});
-});
-
-
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/dashboard');
     return;
   }
   res.render('signup');
