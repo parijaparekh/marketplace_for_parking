53576683881984eb265af6eee20fadc1a7881c37
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-11-27 03:14:44 +0800

    Got addresses to display on dashboard.

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -7,8 +7,8 @@ router.get('/dashboard', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{model: ParkingSlot}, 
-                {model: SlotBooking}]
+      include: [{model: ParkingSlot, include: [Address]}, 
+                {model: SlotBooking, include: [{model: ParkingSlot, include: [Address]}]}]
     });
     const user = userData.get({ plain: true });
     console.log(user);
