b268f35379e846b6c7a77576e353bf7458cd23f0
Ultrapyre <sidneymakhiutao@yahoo.com.hk>
2022-11-26 20:56:55 +0800

    Got the Logout button to work

--- Develop/public/js/logout.js
+++ Develop/public/js/logout.js
@@ -1,6 +1,12 @@
 const logoutHandler = async (event) => {
   event.preventDefault();
-  document.location.replace("/api/user/logout");  
+  const response = await fetch("/api/user/logout", {
+    method: "GET"
+  });
+
+  if (response.ok){
+    document.location.replace("/login");  
+  }
 };
 
 document.querySelector('#logout').addEventListener('click', logoutHandler);
